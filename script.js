1061b33aace0adb822919999c16302cd0c2e9250
Remil Marzan <remilonwheels@gmail.com>
2017-01-04 00:02:03 -0800

    minor edits

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 'use strict';
+var projects = [];
 
 //constructor
-function Project() {
+function Project(projectName, dateBegin, dateEnd, projectIMG, description) {
   this.projectName = projectName;
   this.dateBegin = dateBegin;
   this.dateEnd = dateEnd;
   this.projectIMG = projectIMG;
   this.description = description;
+  this.tags = tags;
 }
