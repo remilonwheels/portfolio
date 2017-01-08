1bbb5f52c34b2488ab0464e2747d9263f1c4f037
Remil Marzan <remilonwheels@gmail.com>
2017-01-07 20:17:30 -0800

    minor edits

--- scripts/app.js
+++ scripts/app.js
@@ -15,16 +15,24 @@ app.handleNavClick = function() {
 app.renderSectionMe = function() {
 }
 
-app.populateProjectFilter = function() {
+app.populateProjectSelectFilter = function() {
   for (var i = 0; i < projectsRaw.length; i++){
     var source = $('#select-project-template').html();
     var templateRender = Handlebars.compile(source);
     var newSelect = templateRender(projectsRaw[i]);
     $('#select-project').append(newSelect);
-
   }
+}
+
+app.createProjectSections = function() {
+  var sections = [];
+  projectsRaw.forEach(function(project) {
+    if (sections.indexOf(project.projectSection) === -1) {
+      sections.push(project.projectSection);
+    }
+  });
 }
 
 app.handleNavToggle();
 app.handleNavClick();
-app.populateProjectFilter();
+// app.populateProjectSelectFilter();
