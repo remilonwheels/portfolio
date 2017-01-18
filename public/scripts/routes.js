334e123e8af2fb7108e017852ec8913ccdbcef16
Remil Marzan <remilonwheels@gmail.com>
2017-01-17 21:42:44 -0800

    implemented spa routes for site nav links

--- public/scripts/routes.js
+++ public/scripts/routes.js
@@ -1,4 +1,8 @@
 'use strict';
 
-page('/', () => console.log('sup'));
+page('/', () => $('body').animate( { scrollTop: $('#section-home').offset().top }, 500));
+page('/me', () => $('body').animate( { scrollTop: $('#section-code').offset().top }, 500));
+page('/work', () => $('body').animate( { scrollTop: $('#section-portfolio').offset().top }, 500));
+page('/contact', () => $('body').animate( { scrollTop: $('#section-contact').offset().top }, 500));
+
 page();
