a7b7ef1e9c6a6f9e0ef2b6f114569cefb5c7dab0
Remil Marzan <remilonwheels@gmail.com>
2017-01-20 07:02:47 -0800

    added a request proxy for the github ajax call

--- server.js
+++ server.js
@@ -1,15 +1,37 @@
 'use strict';
 
 const express = require('express');
+const requestProxy = require('express-request-proxy');
 const app = express();
-
 const PORT = process.env.PORT || 5555;
 
 app.use(express.static('./public'));
 
-app.get('/index.html', function(request, response) {
+function proxyGitHub(request, response) {
+  console.log('Routing GitHub request for', request.params[0]);
+  (requestProxy({
+    url: `https://api.github.com/${request.params[0]}`,
+    headers: {Authorization: `token ${process.env.GITHUB_TOKEN}`}
+  }))(request, response);
+}
+
+//API calls
+app.get('/github/*', proxyGitHub);
+
+//Site navigation
+app.get('/', function(request, response) {
   response.sendFile('index.html', { root : './public'});
 });
+app.get('/me', function(request, response) {
+  response.sendFile('index.html', { root : './public'});
+});
+app.get('/work', function(request, response) {
+  response.sendFile('index.html', { root : './public'});
+});
+app.get('/contact', function(request, response) {
+  response.sendFile('index.html', { root : './public'});
+});
+
 
 app.get('*', function(request, response){
   response.send('404 Error');
